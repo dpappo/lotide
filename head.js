c7a967991c7eaa057b7031754a604819b6a3ad53
Dan Pappo <dpappo@gmail.com>
2021-02-01 12:44:37 -0500

    completed function, including tests

---
+++ head.js
@@ -0,0 +1,25 @@
+// Assert Equal
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const head = function(array) {
+  return array[0];
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+// An array with only one element should still yield that one element as its head
+assertEqual(head([10]), 10);
+
+
+// An empty array should yield undefined as its head
+assertEqual(head([]), undefined);
+
