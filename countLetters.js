2340ca110d0573b5e31dc1065b694b857b73e986
Dan Pappo <dpappo@gmail.com>
2021-02-02 16:44:35 -0500

    solved countLetter problem, including a test

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+// assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  let countObject = {};
+  for (let letter of sentence) {
+    if (countObject[letter] === undefined) {
+      countObject[letter] = 1;
+    } else {
+      countObject[letter] += 1;
+    }
+  }
+
+  console.log(countObject);
+  return countObject;
+};
+
+assertEqual(countLetters("Apple")["p"], 2);
