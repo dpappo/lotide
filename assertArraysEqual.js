f3ab1acc7f8e9de75421717e957913b275d25631
Dan Pappo <dpappo@gmail.com>
2021-02-02 20:40:46 -0500

    updated with different length cases

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,9 @@
 
 
 const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
   for (let i = 0; i < arrayOne.length; i++) {
     if (arrayOne[i] !== arrayTwo[i]) {
       return false;
