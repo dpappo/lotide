86f91e50e8d963df9e005d81185e982a25b61f39
Dan Pappo <dpappo@gmail.com>
2021-02-03 09:30:28 -0500

    solved findKey with tests

---
+++ findKey.js
@@ -0,0 +1,48 @@
+//assert.equal
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+};
+
+
+
+//tests
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+
+
+assertEqual(findKey({
+  "APPL": { value: 69 },
+  "PHR":   { value: 135 },
+  "VCIT":      { value: 96 },
+  "NET":   { value: 84 },
+  "SQ":       { value: 229 },
+  "TWLO":  { value: 389 }
+}, x => x.value > 100), "PHR");
+
+assertEqual(findKey({
+  "APPL": { value: 69 },
+  "PHR":   { value: 135 },
+  "VCIT":      { value: 96 },
+  "NET":   { value: 84 },
+  "SQ":       { value: 229 },
+  "TWLO":  { value: 389 }
+}, x => x.value > 400), undefined);
