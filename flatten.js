99716b3a3731ae958531acaff423bd9ccd8170a6
Dan Pappo <dpappo@gmail.com>
2021-02-04 20:32:01 -0500

    solved flatten lodash

---
+++ flatten.js
@@ -0,0 +1,32 @@
+//assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+const flatten = function(array) {
+  let returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (!Array.isArray(array[i])) {
+      returnArray.push(array[i]);
+    } else {
+      for (let j = 0; j < array[i].length; j++) {
+        returnArray.push(array[i][j]);
+      }
+    }
+  }
+  return returnArray;
+};
+
+assertEqual(eqArrays((flatten([1, 2, [3, 4], 5, [6]])), [1, 2, 3, 4, 5, 6]), true);
