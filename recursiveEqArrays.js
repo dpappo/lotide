5cdcb7f172882610b86314a844c1bea0702e236a
Dan Pappo <dpappo@gmail.com>
2021-02-06 14:43:28 -0500

    Solved recursive array comparison

---
+++ recursiveEqArrays.js
@@ -0,0 +1,26 @@
+//assertEqual
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (Array.isArray(arrayOne[i])) {
+      return eqArrays(arrayOne[i], arrayTwo[i]);
+    } else if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  } return true;
+};
+
+
+assertEqual(eqArrays([1, 2, 3, [4]], [1, 2, 3, [4]]), true); // => true
+assertEqual(eqArrays([1, 2, 3, [4]], [3, 2, 1], [4]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3", ["4"]], ["1", "2", "3", ["4"]]), true); // => true
+assertEqual(eqArrays(["1", "2", "3", ["4"]], ["1", "2", 3, ["4"]]), false); // => false
