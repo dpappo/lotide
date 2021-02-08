14907496062fa24101fbd1930fbf41b0d32b9d5a
Dan Pappo <dpappo@gmail.com>
2021-02-08 09:08:48 -0500

    updated with mocha/chai testing

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,21 +1,40 @@
 const assertEqual = require("../assertEqual");
 const tail = require("../tail");
+const assert = require("chai").assert;
 
 // Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", function () {
+  it("should yield an empty array for an empty array", function () {
+    assert.deepEqual(tail([]), []);
+  });
 
-// An array with only one element should yield an empty array for its tail
-let oneElement = tail(["one"]);
-assertEqual(oneElement.length, 0);
+  it("should yield an empty array for a one-element array", function () {
+    assert.deepEqual(tail(["one"]), []);
+  });
 
-// An empty array should yield an empty array for its tail
-let emptyArray = tail([]);
-assertEqual(emptyArray.length, 0);
+  it("should not change the original array's length", function () {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words);
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("should return 2 elements in a 3 element array", function () {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2);
+  });
+
+  it("should return the 2nd element first in a 3 element array", function () {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse");
+  });
+
+  it("should return the 3rd element second in a 3 element array", function () {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], "Labs");
+  });
+});
