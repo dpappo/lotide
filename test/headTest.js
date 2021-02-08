d0861996ee9499e8225131c0cfa7d4ce60c221ce
Dan Pappo <dpappo@gmail.com>
2021-02-08 08:58:16 -0500

    updated with mocha testing

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,21 @@
 const assertEqual = require("../assertEqual");
 const head = require("../head");
+const assert = require("chai").assert;
 
-// tests;
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-// An array with only one element should still yield that one element as its head
-assertEqual(head([10]), 10);
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
 
-// An empty array should yield undefined as its head
-assertEqual(head([]), undefined);
+  it("returns 'hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns undefined for an empty array []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
