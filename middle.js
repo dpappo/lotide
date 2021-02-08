e184ecda32b83ae8722117196b8894eb8715d0bd
Dan Pappo <dpappo@gmail.com>
2021-02-08 08:50:50 -0500

    updated lotide to use import/export modules with tests in separate folder

--- middle.js
+++ middle.js
@@ -1,20 +1,7 @@
-const eqArrays = function(arrayOne, arrayTwo) {
-  for (let i = 0; i < arrayOne.length; i++) {
-    if (arrayOne[i] !== arrayTwo[i]) {
-      return false;
-    }
-  } return true;
-};
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
-const assertArraysEqual = function(arrayOne, arrayTwo) {
-  if (eqArrays(arrayOne, arrayTwo)) {
-    console.log(`✅✅✅ Arrays are === equal: ${arrayOne} === ${arrayTwo}`);
-  } else {
-    console.log(`🛑🛑🛑 Arrays are not === equal: ${arrayOne} !== ${arrayTwo}`);
-  }
-};
-
-const middle = function(array) {
+const middle = function (array) {
   // some empty variables with the right data types
   let middleNumber = 0;
   let newArray = [];
@@ -29,24 +16,13 @@ const middle = function(array) {
   if (arrayLength % 2 === 0) {
     middleNumber = arrayLength / 2;
     newArray.push(array[middleNumber - 1], array[middleNumber]);
- 
   } else {
- 
     // odd length array;
     middleNumber = Math.floor(arrayLength / 2);
     newArray.push(array[middleNumber]);
   }
-  
+
   return newArray;
 };
 
-// middle([1, 2, 3]), [2];
-// middle([1, 2, 3, 4, 5]);
-// middle([1, 2, 3, 4]), [2, 3]; // => [2
-// middle([1, 2, 3, 4, 5, 6]), [3, 4]; //
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
+module.exports = middle;
